ae46f631c0c29695a1f161d555fc2ccb67201914
Harsh Kumar <kunarharsh76@gmail.com>
2025-07-21 23:16:26 +0530

    Update EXP6-3.cpp

--- EXP6-3.cpp
+++ EXP6-3.cpp
@@ -7,7 +7,10 @@ int main (){
     for (int i=1;i<10;i++){
         if ((i%2)==0){
             continue ;
-        }cout<<i<<endl;
+        }cout<<i<<" ";
     }
  return 0;  
 }
+
+
+//output : 1 3 5 7 9 
